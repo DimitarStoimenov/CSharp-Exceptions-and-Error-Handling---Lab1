c66196c7063fb4f2e00e2c456a11d0e30f98308f
DimitarStoimenov <stoimenovdimitur@yahoo.com>
2022-07-24 19:31:12 +0200

    C#-OOP

---
+++ 01.SquareRoot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            try
+            {
+
+                TrySqrt(num);
+                Console.WriteLine(Math.Sqrt(num));
+
+            }
+            catch (Exception ae)
+            {
+
+                Console.WriteLine(ae.Message); 
+                
+                
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+        public static int TrySqrt(int num)
+        {
+            
+            if (num < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+
+           
+            return num;
+
+        }
+    }
+}
---
+++ 02.EnterNumbers/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnterNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 100;
+            List<string> numbs = new List<string>();
+
+            while (numbs.Count != 10)
+            {
+                try
+                {
+                    string num = Console.ReadLine();
+                    ReadNumber(start, end, num);
+                    numbs.Add(num);
+                    start = int.Parse(num);
+                }
+                catch (ArgumentException re)
+                {
+
+                    Console.WriteLine(re.Message);
+                }
+                catch(Exception ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbs));
+        }
+
+        private static void ReadNumber(int start,  int end, string num)
+        {
+            int number;
+            bool success = int.TryParse(num, out number);
+
+            if (!success)
+            {
+                throw new Exception("Invalid Number!");
+            }
+            
+            else if (number <= start || number > end)
+            {
+                
+                    throw new ArgumentException($"Your number is not in range {start} - 100!");
+            }
+            
+        }
+    }
+}
